a26c32f37d64770177a4ada23c2cea3d599723db
akane <goroneco3446023@gmail.com>
2020-04-17 15:09:31 +0000

    課題提出lalavel9

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'bbb\AAAController@add');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+     Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::get('profile/create', 'Admin\ProfileController@edit');
+});
