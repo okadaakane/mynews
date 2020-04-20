ded07a1e6caf7d9403393448209969957f20fa80
akane <goroneco3446023@gmail.com>
2020-04-20 04:37:20 +0000

    １０課題提出

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
 
 
 Route::group(['prefix' =>'admin'],function(){
-	Route::get('profile/create', 'Admin\profilecontroller@add');
-	Route::get('profile/edit', 'Admin\profilecontroller@edit');
+	Route::get('profile/create', 'Admin\ProfileController@add');
+	Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::get('news/create', 'Admin\NewsController@add');
 });
