e604ee26c43a8dc5239c3aa53c526729b523678a
akane <goroneco3446023@gmail.com>
2020-04-21 09:10:16 +0000

    lalavel　mix知らないタグ多いなあ

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -9,5 +9,21 @@
     </head>
     <body>
         <h1>課題profile編集画面</h1>
+        @extends('layouts.profile')
+
+
+
+        @section('title','プロフィール編集をしよう')
+
+
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>プロフィール編集</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
     </body>
 </html>
