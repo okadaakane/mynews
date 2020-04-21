46066df61f379e813b5e75127b9fbe8a7d86ee17
akane <goroneco3446023@gmail.com>
2020-04-21 04:36:31 +0000

    11の課題8までpush

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -5,9 +5,28 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Profile.create</title>
+        <title>Profile作成画面用</title>
     </head>
     <body>
-        <h1>課題profile.create作成画面</h1>
+        <h1>プロフィール作成画面</h1>
+        
+        
+@extends('layouts.profile')
+
+
+
+@section('title','プロフィール新規作成')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+        
     </body>
 </html>
