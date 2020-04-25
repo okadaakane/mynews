97f1090e1da1171369d14984fe3c6b08d4ef2bd0
akane <goroneco3446023@gmail.com>
2020-04-25 02:51:45 +0000

    課題11

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>profile.edit</title>
-    </head>
-    <body>
-        <h1>課題profile編集画面</h1>
         @extends('layouts.profile')
 
 
@@ -25,5 +14,3 @@
                 </div>
             </div>
         @endsection
-    </body>
-</html>
